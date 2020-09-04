57e3a0347ca31444e03340ed463776405bf464a0
pierre <pierrendomo@yahoo.fr>
2020-09-03 21:55:21 -0400

    first script

--- CENTOS_HARDENING_CIS.sh
+++ CENTOS_HARDENING_CIS.sh
@@ -8,17 +8,17 @@
 
 # Disable mounting of unneeded filesystems CIS 1.1.1 and CIS 3.5
 cat << EOF >> /etc/modprobe.d/CIS.conf
-install cramfs /bin/true
-install freevxfs /bin/true
-install jffs2 /bin/true
-install hfs /bin/true
-install hfsplus /bin/true
-install squashfs /bin/true
-install udf /bin/true
-install dccp /bin/true
-install sctp /bin/true
-install rds /bin/true
-install tipc /bin/true
+yum install cramfs /bin/true
+yum install freevxfs /bin/true
+yum install jffs2 /bin/true
+yum install hfs /bin/true
+yum install hfsplus /bin/true
+yum install squashfs /bin/true
+yum install udf /bin/true
+yum install dccp /bin/true
+yum install sctp /bin/true
+yum install rds /bin/true
+yum install tipc /bin/true
 EOF
 
 
